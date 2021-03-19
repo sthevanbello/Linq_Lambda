869f23ba86de1756ab0a943c1541e19cf4231ca9
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-19 15:03:26 -0300

    Linq using SQL

--- Linq_Lambda/Program.cs
+++ Linq_Lambda/Program.cs
@@ -31,21 +31,43 @@ namespace Linq_Lambda
 
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900);
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name }); ;
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
-            var r5 = r4.Skip(2).Take(4);
-            var r6 = products.First();
-            var r7 = products.Where(p => p.Price > 3000).FirstOrDefault();
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900);
+            var r1 = from p in products where p.Category.Tier == 1 && p.Price < 900 select p;
+
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 = from p in products where p.Category.Name == "Tools" select p.Name;
+
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name }); ;
+            var r3 = from p in products where p.Name[0] == 'C' select new { p.Name, p.Price, CategoryName = p.Category.Name };
+
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 = from p in products where p.Category.Tier == 1 orderby p.Name orderby p.Price select p;
+
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 = (from p in r4 select p).Skip(2).Take(4);
+
+            //var r6 = products.First();
+            var r6 = (from p in products select p).First();
+
+            //var r7 = products.Where(p => p.Price > 3000).FirstOrDefault();
+            var r7 = (from p in products where p.Price > 3000 select p).FirstOrDefault();
+
             var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
+
             var r9 = products.Where(p => p.Id == 30).SingleOrDefault();
+
             var r10 = products.Max(p => p.Price);
+
             var r11 = products.Min(p => p.Price);
+
             var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
+
             var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
+
             var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+
             var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
+
             var r16 = products.GroupBy(p => p.Category);
 
 
@@ -57,23 +79,23 @@ namespace Linq_Lambda
             Print("TIER 1 ORDER BY PRICE THEN NAME Skip 2 Take 4", r5);
             Console.WriteLine("First test1: \n" + r6);
             Console.WriteLine("First test2 FirstOrDefault: \n" + r7 + " -\n");
-            Console.WriteLine($"Single or Default Test2: \n{r9}");
-            Console.WriteLine($"Max value per Price: \n{r10}");
-            Console.WriteLine($"Min value per Price: \n{r11}");
-            Console.WriteLine($"Sum of the prices of products of Category 1: {r12}");
-            Console.WriteLine($"Average of the prices of products of Category 1: {r13}");
-            Console.WriteLine($"Average of the prices of products of Category 5: {r14}");
-            Console.WriteLine($"Agregate Sum of products of category 1: {r15}");
+            //Console.WriteLine($"Single or Default Test2: \n{r9}");
+            //Console.WriteLine($"Max value per Price: \n{r10}");
+            //Console.WriteLine($"Min value per Price: \n{r11}");
+            //Console.WriteLine($"Sum of the prices of products of Category 1: {r12}");
+            //Console.WriteLine($"Average of the prices of products of Category 1: {r13}");
+            //Console.WriteLine($"Average of the prices of products of Category 5: {r14}");
+            //Console.WriteLine($"Agregate Sum of products of category 1: {r15}");
 
-            foreach (IGrouping<Category, Product> group in r16)
-            {
-                Console.WriteLine($"\n{group.Key.Id}    \t{group.Key.Name}    \t{group.Key.Tier}\n");
+            //foreach (IGrouping<Category, Product> group in r16)
+            //{
+            //    Console.WriteLine($"\n{group.Key.Id}    \t{group.Key.Name}    \t{group.Key.Tier}\n");
 
-                foreach (var product in group)
-                {
-                    Console.WriteLine($"{product}");
-                }
-            }
+            //    foreach (var product in group)
+            //    {
+            //        Console.WriteLine($"{product}");
+            //    }
+            //}
 
 
             Console.ReadKey();
